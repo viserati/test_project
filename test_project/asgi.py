f946a3e93bdff96bc869b3d357f5a01deb62a1a0
Keith Turner <turner@keithturner.com>
2020-05-29 22:56:11 -0600

    update

--- test_project/asgi.py
+++ test_project/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for test_project project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
 
 import os
 
